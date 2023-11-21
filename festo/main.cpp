b0a360485c1738835750bbfbc4022abc7b28c879
florian czorniuk <florian.czorniuk@web.de>
2023-11-21 11:25:14 +0100

    21.11.23

--- festo/main.cpp
+++ festo/main.cpp
@@ -1,7 +1,7 @@
 /**
  * @author Niklas Dathe
  * @author Florian Czorniuk
- * @date   09.11.2023
+ * @date   21.11.2023
  */
 
 #include <iostream>
@@ -22,21 +22,40 @@ void q1(FestoTransferSystem *);
 void q2(FestoTransferSystem *);
 void emergency(FestoTransferSystem *, bool *);
 
+void anfangszustand(FestoTransferSystem *, bool *);
+void transportieren(FestoTransferSystem *, bool *);
+void hoehenmessung(FestoTransferSystem *, bool *);
+void anfang(FestoTransferSystem *, bool *);
+void reset(FestoTransferSystem *);
+void weiche(FestoTransferSystem *);
+void rutsche(FestoTransferSystem *);
+void auslauf(FestoTransferSystem *);
 
 int main() {
     FestoTransferSystem festo;
-    bool run = true;
+
     //Boot
     for (int i = 0; i < LAP; ++i) {
         ampelschaltung(&festo);
     }
-
+    bool height = false;
+    bool betriebsbereit = false;
+    bool run = true;
     while(run) {
         festo.updateSensors();
-        start(&festo);
-        stop(&festo);
-        q1(&festo);
-        q2(&festo);
+
+        anfangszustand(&festo, &betriebsbereit);
+        transportieren(&festo, &betriebsbereit);
+        hoehenmessung(&festo, &height);
+        anfang(&festo, &height);
+        weiche(&festo);
+        rutsche(&festo);
+        auslauf(&festo);
+        //start(&festo);
+        //stop(&festo);
+        //q1(&festo);
+        //q2(&festo);
+
         emergency(&festo, &run);
         festo.updateActuators();
     }
@@ -44,41 +63,7 @@ int main() {
 }
 
 
-void ampelschaltung(FestoTransferSystem *festo){
-    //new
-    festo->updateSensors();
-    festo->lampRed.switchOn();
-    festo->lampYellow.switchOff();
-    festo->lampGreen.switchOff();
-    consoleLogBarrierState(festo);
-    festo->updateActuators();
-    sleepForMS(WAIT);
 
-    festo->updateSensors();
-    festo->lampRed.switchOn();
-    festo->lampYellow.switchOn();
-    festo->lampGreen.switchOff();
-    consoleLogBarrierState(festo);
-    festo->updateActuators();
-    sleepForMS(WAIT);
-
-    festo->updateSensors();
-    festo->lampRed.switchOff();
-    festo->lampYellow.switchOff();
-    festo->lampGreen.switchOn();
-    consoleLogBarrierState(festo);
-    festo->updateActuators();
-    sleepForMS(WAIT);
-
-    festo->updateSensors();
-    festo->lampRed.switchOff();
-    festo->lampYellow.switchOn();
-    festo->lampGreen.switchOff();
-    consoleLogBarrierState(festo);
-    festo->updateActuators();
-    sleepForMS(WAIT);
-
-}
 
 
 void consoleLogBarrierState(FestoTransferSystem * festo){
@@ -106,6 +91,151 @@ void consoleLogBarrierState(FestoTransferSystem * festo){
     //cout << "Separator:" << festo->lightbarrierFeedSeparator.getState() << endl;
 
 }
+
+void anfangszustand(FestoTransferSystem *festo, bool * betriebsbereit){
+    if (festo->pushbuttonStart.isPressed() && *betriebsbereit) {
+        cout << "Anfangszustand" << endl;
+        *betriebsbereit = false;
+        festo->ledStart.switchOn();
+        festo->lampGreen.switchOff();
+        festo->lampYellow.switchOff();
+        festo->lampRed.switchOff();
+
+    }
+    else if (festo->pushbuttonStart.isPressed() && !*betriebsbereit) {
+        cout << "Betriebsbereit" << endl;
+        *betriebsbereit = true;
+        festo->lampGreen.switchOn();
+
+    }
+
+
+}
+
+void transportieren(FestoTransferSystem *festo, bool * betriebsbereit) {
+    if (betriebsbereit && festo->lightbarrierBegin.isOpen()) {
+        festo->drive.setSpeed(CONVEYERBELT_RIGHT_SLOW);
+        festo->lampGreen.switchOff();
+        festo->lampYellow.switchOn();
+    }
+
+}
+
+void hoehenmessung(FestoTransferSystem *festo, bool * height){
+    if (festo->lightbarrierHeightSensor.isOpen()){
+        festo->drive.setSpeed(CONVEYERBELT_STOP);
+        festo->lampRed.switchOn();
+        festo->lampYellow.switchOff();
+
+    }
+    if (festo->heightcheck.isHeightCorrect() && festo->lightbarrierHeightSensor.isOpen()){
+        festo->drive.setSpeed(CONVEYERBELT_LEFT_FAST);
+        festo->lampRed.switchOn();
+        festo->lampYellow.switchOn();
+        *height = true;
+
+        festo->ledQ1.switchOn();
+        if (festo->lightbarrierBegin.isOpen()) {
+            festo->ledQ1.switchOff();
+        }
+        cout << "Hoehenmessung: " << festo->heightcheck.isHeightCorrect() << endl; //Gibt nur einen bool aus
+    }
+    else if (!festo->heightcheck.isHeightCorrect() && festo->lightbarrierHeightSensor.isOpen()){
+        festo->drive.setSpeed(CONVEYERBELT_RIGHT_FAST);
+        festo->lampGreen.switchOn();
+        festo->lampYellow.switchOn();
+        festo->lampRed.switchOff();
+        festo->ledQ1.switchOff();
+
+        cout << "Hoehenmessung: " << festo->heightcheck.isHeightCorrect() << endl; //Gibt nur einen bool aus
+
+    }
+
+}
+
+void anfang(FestoTransferSystem *festo, bool * height){
+    if (festo->lightbarrierBegin.isOpen() && height){
+        *height = false;
+
+        festo->drive.setSpeed(CONVEYERBELT_STOP);
+        festo->lampYellow.blink();
+        festo->lampRed.switchOn();
+        festo->ledReset.switchOn();
+
+
+    }
+
+}
+
+void reset(FestoTransferSystem *festo){
+    if (festo->pushbuttonReset.isPressed()){
+        festo->drive.setSpeed(CONVEYERBELT_RIGHT_FAST);
+        festo->lampYellow.switchOn();
+        festo->lampRed.switchOff();
+        festo->ledReset.switchOff();
+    }
+}
+
+void weiche(FestoTransferSystem *festo){
+    if (festo->lightbarrierFeedSeparator.isOpen()){
+        festo->drive.setSpeed(CONVEYERBELT_STOP);
+        festo->lampRed.switchOn();
+        festo->lampYellow.switchOff();
+        festo->lampGreen.switchOff();
+
+        if (!festo->metalcheck.isMetalDetected()) //nicht metallisch
+        {
+            festo->drive.setSpeed(CONVEYERBELT_RIGHT_FAST);
+            festo->lampRed.switchOff();
+            festo->lampYellow.switchOn();
+            festo->feedSeparator.open();
+            festo->ledQ2.switchOn();
+
+        }
+        else { //metallisch
+            festo->drive.setSpeed(CONVEYERBELT_RIGHT_FAST);
+            festo->ledQ2.switchOff();
+            festo->feedSeparator.close();
+            festo->lampYellow.switchOn();
+        }
+    }
+}
+
+void rutsche(FestoTransferSystem *festo){
+    if (festo->lightbarrierEnd.isOpen()){ //rutsche besetzt
+        festo->drive.setSpeed(CONVEYERBELT_STOP);
+        festo->lampRed.switchOn();
+        festo->lampYellow.blink();
+        festo->lampGreen.switchOff();
+    }
+    else { //rutsche frei
+        festo->drive.setSpeed(CONVEYERBELT_STOP);
+        festo->feedSeparator.close();
+        festo->lampRed.switchOn();
+        festo->lampYellow.switchOff();
+        festo->lampGreen.switchOn();
+
+    }
+}
+
+
+void auslauf(FestoTransferSystem *festo) {
+    if (festo->lightbarrierBufferFull.isOpen()) { //auslauf besetzt
+        festo->drive.setSpeed(CONVEYERBELT_STOP);
+        festo->feedSeparator.close();
+        festo->ledQ2.switchOff();
+        festo->lampRed.switchOn();
+        festo->lampYellow.switchOff();
+        festo->lampGreen.switchOn();
+    } else { //auslauf frei
+        festo->drive.setSpeed(CONVEYERBELT_STOP);
+        festo->lampRed.switchOff();
+        festo->lampYellow.switchOn();
+        festo->lampGreen.switchOn();
+    }
+}
+
+
 
 void start(FestoTransferSystem *festo){
     if (festo->pushbuttonStart.isPressed()) {
@@ -153,6 +283,41 @@ void q2(FestoTransferSystem *festo){
         festo->ledQ2.switchOn();
     }
     //festo->ledQ2.setState(!festo->lightbarrierHeightSensor.getState());
+}
+void ampelschaltung(FestoTransferSystem *festo){
+    //new
+    festo->updateSensors();
+    festo->lampRed.switchOn();
+    festo->lampYellow.switchOff();
+    festo->lampGreen.switchOff();
+    consoleLogBarrierState(festo);
+    festo->updateActuators();
+    sleepForMS(WAIT);
+
+    festo->updateSensors();
+    festo->lampRed.switchOn();
+    festo->lampYellow.switchOn();
+    festo->lampGreen.switchOff();
+    consoleLogBarrierState(festo);
+    festo->updateActuators();
+    sleepForMS(WAIT);
+
+    festo->updateSensors();
+    festo->lampRed.switchOff();
+    festo->lampYellow.switchOff();
+    festo->lampGreen.switchOn();
+    consoleLogBarrierState(festo);
+    festo->updateActuators();
+    sleepForMS(WAIT);
+
+    festo->updateSensors();
+    festo->lampRed.switchOff();
+    festo->lampYellow.switchOn();
+    festo->lampGreen.switchOff();
+    consoleLogBarrierState(festo);
+    festo->updateActuators();
+    sleepForMS(WAIT);
+
 }
 
 void emergency(FestoTransferSystem *festo, bool * run){
